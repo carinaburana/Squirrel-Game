88795f550c38a576335754c9246239b30f3c2f37
ina-popina <carinaburana5@gmail.com>
2020-03-23 17:18:59 +0100

    nextStep() randomized switch case

--- src/Entity.java
+++ src/Entity.java
@@ -5,9 +5,9 @@ public abstract class Entity {
     protected XY xy;
 
     public Entity(XY startXy, int id, int energy) {
-        this.xy=startXy;
-        this.id=id;
-        this.energy=energy;
+        this.xy = startXy;
+        this.id = id;
+        this.energy = energy;
     }
 
     public int getId() {
@@ -23,14 +23,46 @@ public abstract class Entity {
     }
 
 
-    public void setXy(XY xy){
-        this.xy=xy;
+    public void setXy(XY xy) {
+        this.xy = xy;
+    }
+
+    public void nextStep() {
+
+        int zufall = (int) (Math.random() * 8) + 1;
+        int x = getXy().getX();
+        int y = getXy().getY();
+
+        switch (zufall) {
+            case 1: //--
+                x--;
+                y--;
+            case 2:
+                y--;
+            case 3:
+                y--;
+                x++;
+            case 4:
+                x--;
+            case 5:
+                x++;
+            case 6:
+                x--;
+                y--;
+            case 7:
+                y++;
+            case 8:
+                x++;
+                y++;
+        }
+
+
     }
 
 
-    public void updateEnergy(int deltaEnergy){
-        energy+=deltaEnergy;   }
-
+    public void updateEnergy(int deltaEnergy) {
+        energy += deltaEnergy;
+    }
 
 
 }
--- src/XY.java
+++ src/XY.java
@@ -1,9 +1,20 @@
 public final class XY {
 
+    private int x;
+    private int y;
 
-     public XY(int x, int y){
-       int x_coord = x;
-        int y_coord =y;
+    public XY(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+
+    public int getX(){
+        return x;
+    }
+
+    public int getY(){
+        return y;
     }
 
 }
