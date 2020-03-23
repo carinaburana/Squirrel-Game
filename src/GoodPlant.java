811c1c66394e494da891c7052537a81ae14c5f6e
ina-popina <carinaburana5@gmail.com>
2020-03-23 10:43:23 +0100

    erneuerter Konstruktor

--- src/GoodPlant.java
+++ src/GoodPlant.java
@@ -1,6 +1,6 @@
 public class GoodPlant extends Entity {
 
-    public GoodPlant(int id, int energy, XY startXy){
+    public GoodPlant(int id,  XY startXy){
         super(startXy);
         energy=100;
 
