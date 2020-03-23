b6e1cbdc12947b58ab0183bc27c57f98a8e83dc3
ina-popina <carinaburana5@gmail.com>
2020-03-23 18:07:20 +0100

    readInput while loop

--- src/ConsoleIO.java
+++ src/ConsoleIO.java
@@ -2,14 +2,18 @@ import java.util.Scanner;
 
 public class ConsoleIO {
 
+    private char move = 'x';
+
     public char readInput(){
         //reads from console
-        Scanner scanner = new Scanner(System.in);
-        System.out.println();
-        System.out.print("press w,a,s,d : " );
-        String input = scanner.next();
-        char move = input.charAt(0);
 
+        while(move !='w' || move !='a' || move !='s' || move != 'd'){
+            Scanner scanner = new Scanner(System.in);
+            System.out.println();
+            System.out.print("use w, a, s, d plus >enter< to move: ");
+            String input = scanner.next();
+            move = input.charAt(0);
+        }
         return move;
 
 
