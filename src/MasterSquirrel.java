a951cf0a92aa29a748d7d341bb28c0f51111fe3b
munavi <muraschko.nata@gmail.com>
2020-03-23 16:16:50 +0100

    Merge branch 'master' of C:\Диск Д\Workspace Intelij\hsa\Programmieren2\squirrelGame with conflicts.

--- src/MasterSquirrel.java
+++ src/MasterSquirrel.java
@@ -4,22 +4,25 @@ public class MasterSquirrel extends Entity {
     public static final int START_ENERGY=1000;
 
     public MasterSquirrel(XY startXy,int id){
-        super(startXy, id, START_ENERGY);
+        super(startXy,id,START_ENERGY);
     }
 
 
     //fürs MiniSquirrels :))
-    public MasterSquirrel(XY startXy, int id, int energy){
-        super(startXy, id, energy);
+    public MasterSquirrel(XY startXy,int id, int energy){
+        super(startXy,id,energy);
     }
 
 
+    protected void spawnMini(XY startXy, int energy){
+        MiniSquirrel newMini = new MiniSquirrel(this.id, startXy, energy);
+    }
+
     public boolean checkMini(Entity entity){
         if(entity instanceof MiniSquirrel) {
-            int entityId=entity.getId();
-
-            return (entityId == this.getId());
-        }return false;
+            return (entity.id == this.id);
+        }
+        return false;
     }
 
 
