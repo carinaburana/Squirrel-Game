5d1ad3da0fd82c58607395562b23c072607a264b
ina-popina <carinaburana5@gmail.com>
2020-03-23 16:58:19 +0100

    mini mit patron

--- src/MasterSquirrel.java
+++ src/MasterSquirrel.java
@@ -1,25 +1,24 @@
 
 public class MasterSquirrel extends Entity {
 
-    public static final int START_ENERGY=1000;
+    public static final int START_ENERGY = 1000;
 
-    public MasterSquirrel(XY startXy,int id){
-        super(startXy,id,START_ENERGY);
+    public MasterSquirrel(XY startXy, int id) {
+        super(startXy, id, START_ENERGY);
     }
 
 
     //fürs MiniSquirrels :))
-    public MasterSquirrel(XY startXy,int id, int energy){
-        super(startXy,id,energy);
+    public MasterSquirrel(XY startXy, int id, int energy) {
+        super(startXy, id, energy);
     }
 
 
-    public boolean checkMini(Entity entity){
-        if(entity instanceof MiniSquirrel) {
-            return (entity.id == this.id);
-        }
-        return false;
-    }
+    public boolean checkMini(Entity entity) {
+        if (entity instanceof MiniSquirrel) {
+            return ((MiniSquirrel) ((MiniSquirrel) entity).getPatron() == this);
+        } else return false;
 
+    }
 
 }
--- src/MiniSquirrel.java
+++ src/MiniSquirrel.java
@@ -1,7 +1,14 @@
 public class MiniSquirrel extends MasterSquirrel{
 
-    public MiniSquirrel(int id, XY startXy, int energy) {
+private  MasterSquirrel patron;
+
+    public MiniSquirrel(int id, XY startXy, int energy, MasterSquirrel patron) {
         super(startXy, id, energy);
+        this.patron= patron;
+    }
+
+    public MasterSquirrel getPatron() {
+        return patron;
     }
 
 
